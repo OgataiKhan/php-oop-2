8744a89553910cc2fdbf36893a12f25384e7c74b
OgataiKhan <badellino.kevin@gmail.com>
2024-01-17 15:49:22 +0100

    feat: add exception handling to name trait

--- Traits/Name.php
+++ Traits/Name.php
@@ -1,26 +1,35 @@
 <?php
 
-trait Name {
+trait Name
+{
   protected $name;
-  
+
   /**
    * set_name
    *
    * @param  mixed $name
    * @return void
+   * @throws Exception if the name is not a valid string.
    */
   public function set_name($name)
   {
+    if (!is_string($name) || empty($name)) {
+      throw new Exception("Name provided is not valid.");
+    }
     $this->name = $name;
   }
-  
+
   /**
    * get_name
    *
    * @return string
+   * @throws Exception if the name has not been set.
    */
   public function get_name()
   {
+    if ($this->name === null) {
+      throw new Exception("Name has not been set.");
+    }
     return $this->name;
   }
 }
